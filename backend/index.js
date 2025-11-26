fc3825a184c8f241477c862ee4fa9d89e7ac8728
nidhipatel3 <jbsnidhipatel14@gmail.com>
2025-11-26 12:24:46 -0500

    updated index.js file updated cors and removed some configuration

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const path = require("path");
 const ejs = require("ejs");
-const mongoose = require("mongoose");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
 
 const userRoute = require("./routes/user");
-const { checkForAuthenticationCookie } = require('./middlewares/authentication');
 const categoryRoute = require("./routes/category");
 const expenseRoute = require("./routes/expense");
 
@@ -16,27 +14,16 @@ const PORT = 8001;
 mongoose.connect("mongodb://localhost:27017/expenseDB")
     .then((e) => console.log("MongoDB connected"));
 
-app.set("view engine", "ejs");
-app.set("views", path.resolve("./views"));
+app.use(cors({
+    origin: 'http://localhost:3000',
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cookieParser());
-app.use(checkForAuthenticationCookie("token"));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(express.static(path.join(__dirname, "public/js")));
 app.use(express.static(path.join(__dirname, '../react-app/build', 'index.html')));
-
-app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-}));
-
-app.get("/", (req, res) => {
-    return res.render("home", {
-        user: req.user,
-    });
-});
 
 app.use("/user", userRoute);
 app.use("/api/category", categoryRoute);
